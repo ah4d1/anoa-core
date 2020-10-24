dd60bd9f9e3d5ed12f950d989c594d5375c7a138
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-24 18:39:00 +0700

    Update ac_string.pas

--- ac_string.pas
+++ ac_string.pas
@@ -1,5 +1,13 @@
 unit ac_string;
 
+{**************************************************************
+*  Part of Anoa-Core by Ahadi Aprianto                        *
+*  See https://github.com/ah4d1/anoa-core                     *
+*  The name "Anoa" came from the name of the species endemic  *
+*  to the island of Sulawesi in Indonesia.                    *
+*  See https://en.wikipedia.org/wiki/Anoa                     *
+**************************************************************}
+
 interface
 
 uses
@@ -13,7 +21,7 @@ type
 
 var
   vacString : tacString;
-  
+
 implementation
 
 function tacString.fcSplit (AString : string; ADelimiter : Char) : TStringList;
@@ -28,3 +36,4 @@ begin
 end;
 
 end.
+
